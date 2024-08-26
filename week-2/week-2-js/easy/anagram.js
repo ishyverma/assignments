48a31478f317ef3aec5d95887921cccc3fd03b32
ishyverma <ishyamver@gmail.com>
2024-08-26 09:51:46 +0530

    done anagram.js assignment

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,6 +5,18 @@
 */
 
 function isAnagram(str1, str2) {
+  let arr1 = str1.toLowerCase().split("")
+  arr1.sort();
+  let sortedString1 = arr1.join("")
+  let arr2 = str2.toLowerCase().split("")
+  arr2.sort();
+  let sortedString2 = arr2.join("")
+
+  if (sortedString1 == sortedString2) {
+    return true
+  } else {
+    return false
+  }
 
 }
 
